340b2e3cd2a4929244e0fcfbce0304b45ceed884
Djunaedi <djunaedi.pakamundi@gmail.com>
2022-11-14 23:56:40 +0800

    hapus dependency

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./css/main.css";
 import DisplayTodos from "./components/DisplayTodos";
 import Todos from "./components/Todos";
 
-import { motion } from "framer-motion";
 function App() {
   return (
     <div className="App">
