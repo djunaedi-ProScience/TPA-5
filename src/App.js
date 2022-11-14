77fa35aee11ee8faa43691e6791af9dd96dd7cfa
Djunaedi <djunaedi.pakamundi@gmail.com>
2022-11-14 23:41:46 +0800

    push pertama

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./css/main.css";
+import DisplayTodos from "./components/DisplayTodos";
+import Todos from "./components/Todos";
+
+import { motion } from "framer-motion";
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <Todos />
+      <DisplayTodos />
+    </div>
+  );
+}
+
+export default App;
